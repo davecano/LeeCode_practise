7b212d035ec9786c106ce1e7a7a9198786b3e6ea
陈旋 <xuanxiao-chen@outlook.com>
2020-01-05 20:58:37 +0800

    新建分支并且提交

--- LeeCode_practise/2020_01_05/Program.cs
+++ LeeCode_practise/2020_01_05/Program.cs
@@ -6,7 +6,7 @@ namespace _2020_01_05
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("DAVECANO,Hello World!");
+            Console.WriteLine("这是第二个分支。如果master算是一个分支的话!");
           
         }
     }
