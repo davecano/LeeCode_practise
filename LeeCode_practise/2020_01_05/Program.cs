5ff9098b11877a035da7d9065ea08508255212ab
陈旋 <xuanxiao-chen@outlook.com>
2020-01-05 20:50:49 +0800

    Revert "delete test"
    This reverts commit 75d1795d8c304714ad1d5e035d7a27e0a7bccb5c.

---
+++ LeeCode_practise/2020_01_05/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _2020_01_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DAVECANO,Hello World!");
+          
+        }
+    }
+}
