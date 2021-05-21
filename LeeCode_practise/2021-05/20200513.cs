8ac9221b12ce0017848b8f51b9e021dcfacd054d
chenxuan <chenxuan@tiansu-china.com>
2021-05-21 17:28:27 +0800

    链表测试

--- LeeCode_practise/2021-05/20200513.cs
+++ LeeCode_practise/2021-05/20200513.cs
@@ -57,42 +57,114 @@ namespace _2021_05
         /// </summary>
         /// <param name="head"></param>
         /// <returns></returns>
-        //public ListNode ReverseList(ListNode head)
-        //{
-        //    //var stack = new Stack<ListNode>();
-        //    // while (head != null)
-        //    // {
-        //    //     stack.Push(head);
-        //    //     head = head.next;
-        //    // }
-        //    // if (stack.Count == 0) return null;
-        //    // var resNode =stack.Pop();
-        //    // var p2 = resNode;
-        //    // while (stack.Count>0)
-        //    // {
-        //    //     p2.next= stack.Pop();
-        //    //     p2 = p2.next;
-        //    // }
-        //    // p2.next = null;
-        //    // return resNode;
-
-        //    ListNode resListNode = null;
-        //    while (head != null)
-        //    {
-        //        //先保存访问的节点的下一个节点，保存起来
-        //        //留着下一步访问的
-        //        ListNode temp = head.next;
-        //        //每次访问的原链表节点都会成为新链表的头结点，
-        //        //其实就是把新链表挂到访问的原链表节点的
-        //        //后面就行了
-        //        head.next = resListNode;
-        //        //更新新链表
-        //        resListNode = head;
-        //        //重新赋值，继续访问
-        //        head = temp;
+        public ListNode ReverseList(ListNode head)
+        {
+            //var stack = new Stack<ListNode>();
+            //while (head != null)
+            //{
+            //    stack.Push(head);
+            //    head = head.next;
+            //}
+            //if (stack.Count == 0) return null;
+            //var resNode = stack.Pop();
+            //var p2 = resNode;
+            //while (stack.Count > 0)
+            //{
+            //    p2.next = stack.Pop();
+            //    p2 = p2.next;
+            //}
+            //p2.next = null;
+            //return resNode;
+            ListNode res =new ListNode(0);
+            var rHead = res;
+            while (head!=null)
+            {
+                var temp = head.next;
+                head.next = rHead.next;
+                rHead.next = head;
+                head = temp;
+            }
+            return res.next;
 
 
-        //    }
-        //}
+        }
+        /// <summary>
+        /// https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnnbp2/
+        /// </summary>
+        /// <param name="l1"></param>
+        /// <param name="l2"></param>
+        /// <returns></returns>
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            //只要有一个为空，就返回另一个
+            if (l1 == null || l2 == null)
+                return l2 == null ? l1 : l2;
+            //把小的赋值给first
+            ListNode first = (l2.val < l1.val) ? l2 : l1;
+            first.next = MergeTwoLists(first.next, first == l1 ? l2 : l1);
+            return first;
+
+            //下面4行是空判断
+            //if (l1 == null)
+            //    return l2;
+            //if (l2 == null)
+            //    return l1;
+            //ListNode dummy = new ListNode(0);
+            //ListNode curr = dummy;
+            //while (l1 != null && l2 != null)
+            //{
+            //    //比较一下，哪个小就把哪个放到新的链表中
+            //    if (l1.val <= l2.val)
+            //    {
+            //        curr.next = l1;
+            //        l1 = l1.next;
+            //    }
+            //    else
+            //    {
+            //        curr.next = l2;
+            //        l2 = l2.next;
+            //    }
+            //    curr = curr.next;
+            //}
+            ////然后把那个不为空的链表挂到新的链表中
+            //curr.next = l1 == null ? l2 : l1;
+            //return dummy.next;
+
+        }
+        /// <summary>
+        /// https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnv1oc/
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public bool IsPalindrome(ListNode head)
+        {
+            //使用递归做一下
+            temp = head;
+            return check(head);
+        }
+        ListNode temp;
+        private bool check(ListNode head)
+        {
+            if (head == null) return true;
+            var res = check(head.next) && head.val == temp.val;
+                temp = temp.next;
+            return res;
+        }
+        /// <summary>
+        /// https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnwzei/
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public bool HasCycle(ListNode head)
+        {
+            ListNode slow = head, fast = head;
+            while (fast!=null&&fast.next!=null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+                if (slow == fast) return true;
+            }
+            return false;
+        }
     }
 }
